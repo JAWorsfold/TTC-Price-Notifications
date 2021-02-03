88ea061e412bd5af3bf1bf383e451892cf618ab5
jworsf01 <32646818+jworsf01@users.noreply.github.com>
2021-02-03 00:51:07 +0000

    csv still todo

--- src/csvhelper.py
+++ src/csvhelper.py
@@ -10,14 +10,12 @@ class CSVHelper():
   @staticmethod
   def dict_to_csv(file, lst_dict):
     df = pd.DataFrame.from_dict(lst_dict)
-    header, mode = False, 'a'
     if not os.path.exists(file):
       dr = '/'.join(file.split('/')[:-1])
       Path(dr).mkdir(parents=True, exist_ok=True)
-      header, mode = True, 'w'
     else:
         df = CSVHelper.remove_duplicates(file, df)
-    df.to_csv(file, index=False)
+    df.to_csv(file, index=False, mode='w', header=True)
 
   @staticmethod
   def add_date_column(date, lst_dict):
@@ -31,9 +29,4 @@ class CSVHelper():
   @staticmethod
   def remove_duplicates(file, df):
     df_read = pd.read_csv(file)
-    print(df_read)
-    df_concat = pd.concat([df_read, df])
-    print(df_concat)
-    df_diff = df_concat.drop_duplicates(keep=False, ignore_index=True)
-    print(df_diff)
-    return df_diff
+    return pd.concat([df_read, df]).drop_duplicates(keep=False, ignore_index=True)
